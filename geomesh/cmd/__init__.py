ab60e9fd0e6b8079d6b5212e8e601c843906a839
Jaime R Calzada <jreniel@gmail.com>
2021-08-05 20:03:31 -0400

    Add missing __init__.py files, update .gitignore

---
+++ geomesh/cmd/__init__.py
@@ -0,0 +1,16 @@
+from geomesh.cmd. import common
+from geomesh.cmd.base import CliComponent
+# from geomesh.cmd.hfun import HfunCli
+from geomesh.cmd.geom import GeomCli
+from geomesh.cmd.build import BuildCli
+from geomesh.cmd.cache import CacheCli
+
+__all__ = [
+    'CliComponent',
+    # 'HfunCli',
+    'GeomCli',
+    'common',
+    'BuildCli',
+    'CacheCli',
+]
+
---
+++ geomesh/db/__init__.py
@@ -0,0 +1,4 @@
+from geomesh.db import orm
+from geomesh.db.cache import Cache
+
+__all__ = ['orm', 'Cache']
